173103e2830da9698976b49700772a479dfb1099
AkashSingh8137 <imakashrandhawa@gmail.com>
2022-10-10 06:33:33 -0400

    French file updated and Different font,fontsize , color and font color is used

--- ezpark/src/main/java/ca/ezlock/it/ezpark/MainActivity.java
+++ ezpark/src/main/java/ca/ezlock/it/ezpark/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
