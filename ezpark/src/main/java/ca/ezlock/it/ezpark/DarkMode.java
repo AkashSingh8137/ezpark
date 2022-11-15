8457f8e193424d8ddc90471ca2b523ddc425e51b
TheCaonima <prasannakc87@gmail.com>
2022-11-14 21:07:07 -0500

    woking on settings page

--- ezpark/src/main/java/ca/ezlock/it/ezpark/DarkMode.java
+++ ezpark/src/main/java/ca/ezlock/it/ezpark/DarkMode.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
